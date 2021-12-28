6be2864793df8a0058753a5b3bb897c5f5fdccbe
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2021-12-28 14:37:19 +0800

    @ (maven_3.6.3-openjdk_11-cn) download url

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-source config.sh
-
 cur_dir=$(pwd)
 
+maven_version="3.6.3"
+image_tag="maven-${maven_version}-openjdk-11-cn"
+maven_package="apache-maven-${maven_version}-bin.tar.gz"
+maven_dir="apache-maven-${maven_version}"
+jdk_package="microsoft-jdk-11.0.13.8.1-linux-x64.tar.gz"
+jdk_dir="jdk-11.0.13+8"
+jdk_safe_dir="jdk-11.0.13.8"
+
 if [ ! -f "${cur_dir}/${maven_package}" ]; then
-  wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/${maven_package} -O "${cur_dir}/${maven_package}"
+  wget "https://dlcdn.apache.org/maven/maven-3/${maven_version}/binaries/${maven_package}" -O "${cur_dir}/${maven_package}"
 fi
 
 if [ ! -f "${cur_dir}/${jdk_package}" ]; then
-  wget https://aka.ms/download-jdk/${jdk_package} -O "${cur_dir}/${jdk_package}"
+  wget "https://aka.ms/download-jdk/${jdk_package}" -O "${cur_dir}/${jdk_package}"
 fi
 
 tar -xzf "${cur_dir}/${maven_package}"
--- config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-image_tag="maven-3.6.3-openjdk-11-cn"
-maven_package="apache-maven-3.6.3-bin.tar.gz"
-maven_dir="apache-maven-3.6.3"
-jdk_package="microsoft-jdk-11.0.13.8.1-linux-x64.tar.gz"
-jdk_dir="jdk-11.0.13+8"
-jdk_safe_dir="jdk-11.0.13.8"
